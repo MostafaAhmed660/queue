f9d3766dd447d5ea5dfa0ee15a49ced57b0db856
MostafaAhmed660 <59110598+MostafaAhmed660@users.noreply.github.com>
2020-04-06 13:35:37 +0200

    Add files via upload

---
+++ linkedqueue.java
@@ -0,0 +1,23 @@
+package eg.edu.alexu.csd.datastructure.queue;
+
+public class linkedqueue implements ILinkedBased {
+    linkedlist s = new linkedlist();
+
+    public void enqueue(Object item){
+        s.add(item);
+    }
+
+    public Object dequeue(){
+        Object e = s.get(0);
+        s.remove(0);
+        return e ;
+    }
+
+    public boolean isEmpty(){
+        return s.isEmpty();
+    }
+
+    public int size(){
+        return s.size();
+    }
+}
